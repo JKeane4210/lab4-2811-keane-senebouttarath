2b47eecc4d7a7789d2ba858ca86b62c313708e04
senebouttarathk <senebouttarathk@msoe.edu>
2022-01-06 09:40:19 -0600

    Constants change BigBee

--- src/bee_simulator/BigBee.java
+++ src/bee_simulator/BigBee.java
@@ -4,10 +4,10 @@ public class BigBee extends Bee {
 
     //---------------- CONSTANTS ----------------\\
 
-    private static final int COLLISION_RADIUS = 10;
-    private static final int MAX_ENERGY = 10;
-    private static final int INIT_ENERGY = 10;
-    private static final int MOVE_DISTANCE = 30;
+    private static final int COLLISION_RADIUS = 30;
+    private static final int MAX_ENERGY = 20;
+    private static final int INIT_ENERGY = 20;
+    private static final int MOVE_DISTANCE = 10;
     private static final String IMG = "./Assets/bee-1.jpg";
     private static final String DESC = "BIG BOY";
 
