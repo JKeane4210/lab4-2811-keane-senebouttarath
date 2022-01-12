faa08502ed6b23d575f99577f850690e075b5e77
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2022-01-12 09:29:10 -0600

    Balancing (speed buff bees 50%)

--- src/bee_simulator/BigBee.java
+++ src/bee_simulator/BigBee.java
@@ -21,7 +21,7 @@ public class BigBee extends Bee {
     private static final int COLLISION_RADIUS = 20;
     private static final int MAX_ENERGY = 300;
     private static final int INIT_ENERGY = 200;
-    private static final int MOVE_DISTANCE = 10;
+    private static final int MOVE_DISTANCE = 15;
     private static final String IMG = "./Assets/bee.png";
     private static final String DESC = "A grown up bee with more health, but it is slower than the baby bee.";
 
